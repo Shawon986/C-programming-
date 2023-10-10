994137b8c17c29fd20c600aac63bbf6615bc9e5c
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-10-11 00:39:22 +0600

    data type and escape sequence

--- test/main.c
+++ test/main.c
@@ -4,7 +4,16 @@ int main()
 {
     printf("Hello world \n");//print anything
     printf("Taizhou University \n");
-    printf("18658607019");
+    printf("18658607019 \a");
+
+    int num1;
+    int num2;
+    float num3;
+
+    num1=20;
+    num2=50;
+    num3=34.56;
+
     getch();
 }
 /*this is
